8ab3c022ada4c8e97ed0149421ab73dd73c4ff2d
dwakna <dngyeong@naver.com>
2021-06-10 11:40:32 +0900

    file I/O 처리

--- main.cpp
+++ main.cpp
@@ -1,26 +1,20 @@
 #include "storageIO.h"
+#include "voms_log.h"
 
 int main()
 {
-    StorageIO sio("config.txt");
-    vector<string> pathList = sio.pathList_;
+	StorageIO sio("config.txt");
+	vector<string> pathList = sio.pathList_;
 
-
-    for(auto &path : pathList)
-    {
-
-
-        vector<string> fileList = sio.getFileList(path);
-#if 1
-        for(int i = 0; i < fileList.size(); i++)
-        {
-            string finalPath = path + "/" + fileList[i];
-            sio.readImg(finalPath);
-            sio.writeImg(fileList[i]);
-        }
-#endif
-    }
-
-
-    return 0;
+	for(auto &path : pathList)
+	{
+		vector<string> fileList = sio.getFileList(path);
+		for(int i = 0; i < fileList.size(); i++)
+		{
+			string finalPath = path + "/" + fileList[i];
+			sio.readImg(finalPath);
+			sio.writeImg(fileList[i]);
+		}
+	}
+	return 0;
 }
--- storageIO.cpp
+++ storageIO.cpp
@@ -1,126 +1,116 @@
+#include "storageIO.h"
+StorageIO::StorageIO(string config)
+{
+	ifstream fin;
+	fin.open(config);
+	string line;
+	
+	//while(!fin.eof())
+	while(fin.peek() != EOF)
+	{
+		getline(fin, line);
+		pathList_.push_back(line);
+	}
+	fin.close();
+}
+
 bool StorageIO::IsImg(string fileName)
 {
-    string ext = fileName.substr(fileName.find_last_of(".") + 1);
-    if(ext == "jpg")
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
+	string ext = fileName.substr(fileName.find_last_of(".") + 1);
+	if(ext == "jpg")
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
 }
 
 vector<string> StorageIO::getFileList(string pathName)
 {
-    DIR* dirp = opendir(pathName.c_str());
-    struct dirent *dp=NULL;
+	DIR* dirp = opendir(pathName.c_str());
+	struct dirent *dp=NULL;
 
-    vector<string> fileList;
+	vector<string> fileList;
 
-    if(dirp == NULL)
-    {
-        //T_LOG();
-        return fileList;
-    }
-
-    while((dp=readdir(dirp)) != NULL)
-    {
-        string fileName = dp->d_name;
-        if(IsImg(fileName)==true)
-        {
-            fileList.push_back(dp->d_name);
-        }
-    }
-    closedir(dirp);
-    return fileList;
+	if(dirp == NULL)
+	{
+		//T_LOG();
+		return fileList;
+	}
+	while((dp=readdir(dirp)) != NULL)
+	{
+		string fileName = dp->d_name;
+		if(IsImg(fileName)==true)
+		{
+			fileList.push_back(fileName);
+		}
+	}
+	closedir(dirp);
+	return fileList;
 }
 
 void StorageIO::readImg(string fileName)
 {
-    ifstream ifs;
-    cout << fileName << endl;
-    ifs.open(fileName, ios::binary);
-    ifs.seekg(0, ios::end);
-    length_ = ifs.tellg();
-    ifs.seekg(0, ios::beg);
-    char *buf = new char[length_];
-    ifs.read(buf, length_);
-    ifs.close();
-    //char membuf[lengthaverage];
-    memset(membuf_, 0, sizeof(membuf_));
-    memcpy(membuf_, buf, sizeof(buf));
-    delete []buf;
+	ifstream ifs;
+	ifs.open(fileName, ios::binary);
+	ifs.seekg(0, ios::end);
+	this->length_ = ifs.tellg();
+	ifs.seekg(0, ios::beg);
+	char *buf = new char[length_];
+	ifs.read(buf, length_);
+	ifs.close();
+	//char membuf[lengthaverae];
+
+	membuf_ = new char[length_ + 1];
+	memset(membuf_,  0, sizeof(membuf_));
+	memcpy(membuf_, buf, length_);
+	delete []buf;
 }
 
 void StorageIO::writeImg(string name)
 {
-    ofstream ofs;
-    int num = name.find_last_of(".");
-    string realName = name.substr(0, num);
-    ofs.open("/home/ict01/workplace/OJT/StorageIO/copy/" + realName  + "_update.jpg" , ios::binary);
-    //ofs.write(buf, length);
-"storageIO.cpp" 105L, 1797C                                                                                                         79,2-5        38%
+	ofstream ofs;
+	int num = name.find_last_of(".");
+	string realName = name.substr(0, num);
+	ofs.open("/home/ict01/workplace/OJT/StorageIO/copy/" + realName  + "_update.jpg" , ios::binary);
 
-    while((dp=readdir(dirp)) != NULL)
-    {
-        string fileName = dp->d_name;
-        if(IsImg(fileName)==true)
-        {
-            fileList.push_back(dp->d_name);
-        }
-    }
-    closedir(dirp);
-    return fileList;
+	//ofs.open("/home/ict01/workplace/OJT/StorageIO/copy/" + name , ios::binary);
+	if(ofs.fail())
+	{
+		cerr << "Error : " << strerror(errno)<<endl;
+	}
+
+	ofs.write(membuf_, length_);
+	ofs.close();
+
+	if(membuf_)
+	{
+		delete []membuf_;
+		membuf_ = nullptr;
+	}
 }
 
-void StorageIO::readImg(string fileName)
-{
-    ifstream ifs;
-    cout << fileName << endl;
-    ifs.open(fileName, ios::binary);
-    ifs.seekg(0, ios::end);
-    length_ = ifs.tellg();
-    ifs.seekg(0, ios::beg);
-    char *buf = new char[length_];
-    ifs.read(buf, length_);
-    ifs.close();
-    //char membuf[lengthaverage];
-    memset(membuf_, 0, sizeof(membuf_));
-    memcpy(membuf_, buf, sizeof(buf));
-    delete []buf;
-}
-
-void StorageIO::writeImg(string name)
-{
-    ofstream ofs;
-    int num = name.find_last_of(".");
-    string realName = name.substr(0, num);
-    ofs.open("/home/ict01/workplace/OJT/StorageIO/copy/" + realName  + "_update.jpg" , ios::binary);
-    //ofs.write(buf, length);
-    ofs.write(membuf_[0], length_);
-    ofs.close();
-}
-
+# if 0
 bool StorageIO::checkFile()
 {
-    return true;
+	return true;
 }
 
 bool StorageIO::checkTime()
 {
-    const char *path = "";
-    struct stat attr;
+	const char *path = "";
+	struct stat attr;
 
-    if(stat(path, &attr) < 0)
-    {
-        cout << "stat error" << endl;
-        exit(0);
-    }
-    time_t mtime = attr.st_mtime;
-    cout << "Last modified time : " << ctime(&mtime) << endl;
+	if(stat(path, &attr) < 0)
+	{
+		cout << "stat error" << endl;
+		exit(0);
+	}
+	time_t mtime = attr.st_mtime;
+	cout << "Last modified time : " << ctime(&mtime) << endl;
 
-    return true;
+	return true;
 }
-    }
-}
+#endif
